f854169bc898e89bdec9008ce49876afeac9edc6
Leo Breneis <114604133+Leo-Breneis@users.noreply.github.com>
2025-02-12 13:16:58 +0100

    timetable component

--- user-frontend/src/app/app.routes.ts
+++ user-frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { StartPageComponent } from './start-page/start-page.component';
 import { FestivalStartComponent } from './festival-start/festival-start.component';
 import { UserStartComponent } from './user-start/user-start.component';
+import { TimetableComponent } from './timetable/timetable.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/start-page', pathMatch: 'full' },
   { path: 'start-page', component: StartPageComponent },
   { path: 'festival-start', component: FestivalStartComponent },
-  { path: 'user-start', component: UserStartComponent }
+  { path: 'user-start', component: UserStartComponent },
+  { path: 'timetable', component: TimetableComponent }
 ];
