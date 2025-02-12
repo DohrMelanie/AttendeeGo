d1925f76066ade58c7997be972489bd1b4267bc3
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2025-02-12 11:40:34 +0100

    fixed start page of user

---
+++ user-frontend/src/app/festival-start/festival-start.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-festival-start',
+  standalone: true,
+  imports: [],
+  templateUrl: './festival-start.component.html',
+  styleUrl: './festival-start.component.css'
+})
+export class FestivalStartComponent {
+
+}
