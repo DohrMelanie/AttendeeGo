37199f29cc0c7b87bf2f2d1bb00853796376ce04
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2025-02-12 10:22:12 +0100

    added styling

--- user-frontend/src/app/app.routes.ts
+++ user-frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { StartPageComponent } from './start-page/start-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/start-page', pathMatch: 'full' },
+  { path: 'start-page', component: StartPageComponent },
+];
