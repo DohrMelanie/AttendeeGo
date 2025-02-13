b8b0c4c0536af8805d83715dc3c86d7c757779d8
Eros Klein <e.klein@students.htl-leonding.ac.at>
2025-02-13 11:57:52 +0100

    add light-mode

--- admin-frontend/tailwind.config.js
+++ admin-frontend/tailwind.config.js
@@ -1,70 +1,73 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{html,ts}'],
-    theme: {
-      extend: {
-        colors: {
-          foreground: {
-          100: "#fdfeff",
-          200: "#fbfefe",
-          300: "#f8fdfe",
-          400: "#f6fdfd",
-          500: "#f4fcfd",
-          600: "#c3caca",
-          700: "#929798",
-          800: "#626565",
-          900: "#313233"
-          },
-          background: {
-          100: "#cdd2d2",
-          200: "#9ba5a5",
-          300: "#697879",
-          400: "#374b4c",
-          500: "#051e1f",
-          600: "#041819",
-          700: "#031213",
-          800: "#020c0c",
-          900: "#010606"
-          },
-          primary: {
-          100: "#d1e2e5",
-          200: "#a3c6cb",
-          300: "#76a9b0",
-          400: "#488d96",
-          500: "#1a707c",
-          600: "#155a63",
-          700: "#10434a",
-          800: "#0a2d32",
-          900: "#051619"
-          },
-          secondary: {
-          100: "#d3ccdf",
-          200: "#a699be",
-          300: "#7a669e",
-          400: "#4d337d",
-          500: "#21005d",
-          600: "#1a004a",
-          700: "#140038",
-          800: "#0d0025",
-          900: "#070013"
-          },
-          accent: {
-          100: "#d4f2f8",
-          200: "#a9e6f1",
-          300: "#7ed9eb",
-          400: "#53cde4",
-          500: "#28c0dd",
-          600: "#209ab1",
-          700: "#187385",
-          800: "#104d58",
-          900: "#08262c"
-},
-         },
-        fontFamily: {
-          sans: ['Roboto', 'sans-serif'],
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        foreground: {
+          DEFAULT: 'var(--foreground-500)',
+          100: 'var(--foreground-100)',
+          200: 'var(--foreground-200)',
+          300: 'var(--foreground-300)',
+          400: 'var(--foreground-400)',
+          500: 'var(--foreground-500)',
+          600: 'var(--foreground-600)',
+          700: 'var(--foreground-700)',
+          800: 'var(--foreground-800)',
+          900: 'var(--foreground-900)',
+        },
+        background: {
+          DEFAULT: 'var(--background-500)',
+          100: 'var(--background-100)',
+          200: 'var(--background-200)',
+          300: 'var(--background-300)',
+          400: 'var(--background-400)',
+          500: 'var(--background-500)',
+          600: 'var(--background-600)',
+          700: 'var(--background-700)',
+          800: 'var(--background-800)',
+          900: 'var(--background-900)',
+        },
+        primary: {
+          DEFAULT: 'var(--primary-500)',
+          100: 'var(--primary-100)',
+          200: 'var(--primary-200)',
+          300: 'var(--primary-300)',
+          400: 'var(--primary-400)',
+          500: 'var(--primary-500)',
+          600: 'var(--primary-600)',
+          700: 'var(--primary-700)',
+          800: 'var(--primary-800)',
+          900: 'var(--primary-900)',
+        },
+        secondary: {
+          DEFAULT: 'var(--secondary-500)',
+          100: 'var(--secondary-100)',
+          200: 'var(--secondary-200)',
+          300: 'var(--secondary-300)',
+          400: 'var(--secondary-400)',
+          500: 'var(--secondary-500)',
+          600: 'var(--secondary-600)',
+          700: 'var(--secondary-700)',
+          800: 'var(--secondary-800)',
+          900: 'var(--secondary-900)',
+        },
+        accent: {
+          DEFAULT: 'var(--accent-500)',
+          100: 'var(--accent-100)',
+          200: 'var(--accent-200)',
+          300: 'var(--accent-300)',
+          400: 'var(--accent-400)',
+          500: 'var(--accent-500)',
+          600: 'var(--accent-600)',
+          700: 'var(--accent-700)',
+          800: 'var(--accent-800)',
+          900: 'var(--accent-900)',
         },
       },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
     },
-    plugins: [require('tailwindcss-animated')],
-  };
-  
+  },
+  plugins: [require('tailwindcss-animated')],
+};
