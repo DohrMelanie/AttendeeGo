a6421e10923c8c602ef8aa96a36059563fe1db38
Leo Breneis <114604133+Leo-Breneis@users.noreply.github.com>
2025-02-13 11:34:14 +0100

    resolved merge conficts

--- user-frontend/src/app/navbar-festival/navbar-festival.component.ts
+++ user-frontend/src/app/navbar-festival/navbar-festival.component.ts
@@ -11,11 +11,8 @@ import { Router } from '@angular/router';
 export class NavbarFestivalComponent {
   activeTab = signal<string>(localStorage.getItem('activeTab') || 'festival-start');  protected router = inject(Router);
   navigateTo(path: string) {
-  activeTab = signal<string>(localStorage.getItem('activeTab') || 'start-festival');  protected router = inject(Router);
-  public navigateTo(path: string) {
     this.activeTab.set(path);
     localStorage.setItem('activeTab', path);
     this.router.navigate(['/' + path]);
-  }
-  
+  }  
 }
