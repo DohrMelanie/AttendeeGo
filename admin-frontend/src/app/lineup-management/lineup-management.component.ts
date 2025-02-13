9500f9dc31daa0dcb6b43f7bfeff681fa6ff03e6
Eros Klein <e.klein@students.htl-leonding.ac.at>
2025-02-13 10:49:02 +0100

    nav centered + acordion working

---
+++ admin-frontend/src/app/lineup-management/lineup-management.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+@Component({
+  selector: 'app-lineup-management',
+  standalone: true,
+  imports: [MatExpansionModule],
+  templateUrl: './lineup-management.component.html',
+  styleUrl: './lineup-management.component.css'
+})
+export class LineupManagementComponent {
+
+}
