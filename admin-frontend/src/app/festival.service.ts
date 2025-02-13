ac12dc842621bf14dbc2a5a0e914ac2415b4454d
Eros Klein <e.klein@students.htl-leonding.ac.at>
2025-02-13 20:53:36 +0100

    add more festivals

--- admin-frontend/src/app/festival.service.ts
+++ admin-frontend/src/app/festival.service.ts
@@ -21,12 +21,30 @@ export class FestivalService {
     {
       id: 1,
       name: 'Regex Rave',
-      description: 'Best party ever asd asdasd ad das das dasd sa dfasd fsady daf gdwsf asf dsgas fdsgafdgas gsdasf gfsasfdgsdeasfgrwafsa',
+      description: 'Code meets chaos! Lil C headlines with Critical Stage-Overflow at "Lil C’s Terminal". Bass drops harder than a failed regex match—ready to debug the dance floor?',
       ort: 'Wels',
-      startDate: '17.02.2025',
-      endDate: '20.02.2025',
+      startDate: '1.04.2025',
+      endDate: '2.04.2025',
       imageUrl: '/assets/img/lilc.webp'
-    }
+    },
+    {
+      id: 2,
+      name: 'BitBash Festival',
+      description: 'A festival where beats and bytes collide! Headlined at the "Infinite Loop Stage", expect nonstop energy and pure digital mayhem. Ready to crash the dancefloor?',
+      ort: 'Edinburgh',
+      startDate: '12.04.2025',
+      endDate: '15.02.2025',
+      imageUrl: '/assets/img/stage-overflow.webp'
+    },
+    {
+      id: 2,
+      name: 'Data-Drop Festival',
+      description: 'Where music uploads straight to your soul! Located at SQL – the Synthesized Quality Legends Stage, expect high-speed beats, heavy bass queries, and no downtime. Ready to commit to the groove?',
+      ort: 'Saudorf',
+      startDate: '05.10.2025',
+      endDate: '06.07.2025',
+      imageUrl: '/assets/img/sql.webp'
+    },
   ]
 
   public async getAllFestivals() : Promise<Festival[]> {
