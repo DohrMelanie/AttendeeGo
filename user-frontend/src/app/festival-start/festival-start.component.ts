31a48c1317a589ce44f2cc921c192562a114aeaf
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2025-02-13 20:31:54 +0100

    added lil c and fixed overflow

--- user-frontend/src/app/festival-start/festival-start.component.ts
+++ user-frontend/src/app/festival-start/festival-start.component.ts
@@ -2,18 +2,18 @@ import { Component, inject } from '@angular/core';
 import { NavbarFestivalComponent } from '../navbar-festival/navbar-festival.component';
 import { Router } from '@angular/router';
 import { BellComponent } from '../bell/bell.component';
-import { BackComponent } from "../back/back.component";
 
 @Component({
   selector: 'app-festival-start',
   standalone: true,
-  imports: [NavbarFestivalComponent, BellComponent, BackComponent],
+  imports: [NavbarFestivalComponent, BellComponent ],
   templateUrl: './festival-start.component.html',
 })
 export class FestivalStartComponent {
   protected router = inject(Router);
 
   artists = [
+    { name: 'LIL C', image: 'artistsFestival/lilc.png' },
     { name: 'ASAP ROCKY', image: 'artistsFestival/rocky.png' },
     { name: 'FUTURE', image: 'artistsFestival/future.png' },
     { name: 'PESO PLUMA', image: 'artistsFestival/peso.png' },
