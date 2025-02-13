f05dee70b59f46a1ffe07af955b583b899e66e41
Leo Breneis <114604133+Leo-Breneis@users.noreply.github.com>
2025-02-13 10:06:47 +0100

    add map

--- user-frontend/src/app/map/map.component.ts
+++ user-frontend/src/app/map/map.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
 import { NavbarFestivalComponent } from '../navbar-festival/navbar-festival.component';
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   standalone: true,
   imports: [NavbarFestivalComponent],
   templateUrl: './map.component.html',
-  styleUrl: './map.component.css'
+  styleUrl: './map.component.css',
 })
-export class MapComponent {
-
+export class MapComponent {  
+  
 }
