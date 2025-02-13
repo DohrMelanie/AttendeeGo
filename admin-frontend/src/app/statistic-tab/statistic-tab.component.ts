10008162e06c1d8aafcee6357ab4591cf06cbea1
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2025-02-13 15:01:15 +0100

    fixed a bug

--- admin-frontend/src/app/statistic-tab/statistic-tab.component.ts
+++ admin-frontend/src/app/statistic-tab/statistic-tab.component.ts
@@ -5,7 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './statistic-tab.component.html',
-  styleUrl: './statistic-tab.component.css'
 })
 export class StatisticTabComponent {
 
